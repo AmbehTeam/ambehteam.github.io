34718ed3b015f8616f73c0051da063c584928374
Yaseen Rajabi <52135608+yaseenrajabi@users.noreply.github.com>
2020-02-09 18:47:30 +0200

    Revert "k"
    This reverts commit 231826284f3a9d917f2b7e5b90f86494f9694794.

--- assets/footer.php
+++ assets/footer.php
@@ -31,3 +31,5 @@
   				<h5 style="font-family: 'Cinzel', serif; font-size:13.28px; font-weight: bold;">&copy; 2019 by Ambeh Team , Inc. 
   				All Rights Reserved.</h5>
       </div>
+	</body>
+</html>
